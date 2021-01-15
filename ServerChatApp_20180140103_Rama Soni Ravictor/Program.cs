2ce9b4ca9cb16e8ca19646b71381af8d396d35b5
Rama Soni Ravictor <55401842+ravictor21@users.noreply.github.com>
2021-01-15 23:15:39 +0700

    Add project files.

---
+++ ServerChatApp_20180140103_Rama Soni Ravictor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WCFService_2Way_20180140103;
+
+namespace ServerChatApp_20180140103_Rama_Soni_Ravictor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObject = null;
+            try
+            {
+                hostObject = new ServiceHost(typeof(ServiceCallback));
+                hostObject.Open();
+
+                Console.WriteLine("Server On, Ready to use");
+                Console.ReadLine();
+
+                hostObject.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObject = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
